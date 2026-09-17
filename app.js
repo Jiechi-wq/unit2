fa40c0fbd06cab6751c40ec520f5aadabfcddd3a
jiechi <jiechic@nycstudents.net>
2026-09-17 12:38:09 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+/* const isLoggedIn = true;
+const role = "admin";
+if (isLoggedIn === true){
+    if (role === "admin") { 
+        console.log("Mod")
+    }else(
+        console.log("no")
+    ) 
+} */
+
+let age = 35;
+let citizen = true;
+
+if (age >= 35 && citizen === true){
+    console.log("run for president")
+}
+
+
+
+
+
+
+
+
+
+
