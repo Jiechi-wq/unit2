79d3672ab16f26dce27d5242ddd586210e3c5cfe
jiechi <jiechic@nycstudents.net>
2026-09-23 12:32:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,27 @@ if (age >= 35 && citizen === true){
 }
  */
 
+function classifyNumber(num){
+    if (num === 0)
+        console.log("zero")
+    else if (num % 2 === 0 && num>0){
+        console.log(num, "positive even")
+    }
+    else if (num % 2 === 1 && num>0){
+        console.log(num, "positive odd")
+    }
+    else if (num % 2 === 0 && num<0){
+        console.log(num, "negative even")
+    }
+    else if (num % 2 === 1 && num<0){
+        console.log(num, "negative odd")
+    }
+}
+classifyNumber(0)
+classifyNumber(4)
+classifyNumber(7)
+classifyNumber(-4)
+classifyNumber(-7)
 
 
 
